e15659639c2a44c3b688e9862798fe811d64c2df
NH Phat <nhphat8995@gmail.com>
2016-10-01 18:23:38 +0700

    Edit - Update blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,19 +3,43 @@ class PostsController < ApplicationController
 		@post = Post.all.order('created_at DESC')
 	end
 
-	def new
-	end
-
-	def create
-		@post = Post.new(post_params)
-		@post.save
-
-		redirect_to @post
-	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
+
+
+	def new
+		@post = Post.new
+	end
+
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to @post
+		else
+    		render 'new'
+		end
+	end
+
+
+	def update
+		@post = Post.new(post_params)
+		if @post.update(post_params)
+			redirect_to @post
+		else
+    		render 'edit'
+		end
+	end
+
+
 	private
 		def post_params
 			params.require(:post).permit(:title, :body)
